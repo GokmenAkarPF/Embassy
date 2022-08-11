55b06ffd2b923866cf7cc517e0aea94cb434985f
GokmenAkarPF <gokmen@propertyfinder.ae>
2022-08-11 12:18:59 +0300

    Change package description

--- Package.swift
+++ Package.swift
@@ -3,6 +3,20 @@ import PackageDescription
 
 let package = Package(
     name: "Embassy",
+    platforms: [.iOS(.v14)],
     products: [.library(name: "Embassy", targets: ["Embassy"])],
-    targets: [.target(name: "Embassy", path: "./Sources")]
+    dependencies: [
+        .package(
+            name: "Ambassador",
+            url: "https://github.com/envoy/Ambassador",
+            .upToNextMinor(from: Version(4, 0, 5))
+        )
+    ],
+    targets: [
+        .target(
+            name: "Embassy",
+            dependencies: ["Ambassador"],
+            path: "./Sources"
+        )
+    ]
 )
